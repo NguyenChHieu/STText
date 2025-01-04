4f3d46e3b5c8a050bba60271edc976ee9ce5e30e
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-04 11:12:09 +0700

    set controller

---
+++ AudioConverter/src/main/java/com/audio/audioconverter/TranscriptionController.java
@@ -0,0 +1,9 @@
+package com.audio.audioconverter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/transcribe")
+public class TranscriptionController {
+}
