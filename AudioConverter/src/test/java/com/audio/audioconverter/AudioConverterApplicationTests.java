4725a4e3c83b2f7edd783d34cd7107f896f1ae11
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-04 11:08:24 +0700

    add

---
+++ AudioConverter/src/test/java/com/audio/audioconverter/AudioConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.audioconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
